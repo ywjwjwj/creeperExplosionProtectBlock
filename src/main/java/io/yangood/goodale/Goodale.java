3f94d65e415ceaef74695e2d98d68716f8c9029e
yangood <1978505627@qq.com>
2023-12-08 16:18:31 +0800

    插件信息调整

--- src/main/java/io/yangood/goodale/Goodale.java
+++ src/main/java/io/yangood/goodale/Goodale.java
@@ -1,5 +1,6 @@
 package io.yangood.goodale;
 
+import io.papermc.paper.plugin.configuration.PluginMeta;
 import io.yangood.goodale.listen.CustomListen;
 import io.yangood.goodale.utils.Util;
 import net.kyori.adventure.text.Component;
@@ -11,10 +12,16 @@ public final class Goodale extends JavaPlugin {
     @Override
     public void onEnable() {
         Util.init(getServer());
+
+        PluginMeta pluginMeta = getPluginMeta();
+        String pluginVersion = pluginMeta.getVersion();
+        String serverVersion = getServer().getVersion();
         // Plugin startup logic
         getServer().sendMessage(
             Component.text("[Goodale]", TextColor.fromHexString("#36BE43"))
+                .append(Component.text("插件bug，邮件联系 ywj@yangood.top!", TextColor.fromHexString("#be0013")))
                 .append(Component.text("Hello, 插件已装配!", TextColor.fromHexString("#36BE43")))
+                .append(Component.text("当前服务器版本【" + serverVersion + "】, 请确认版本与插件版本【" + pluginVersion + "】匹配"))
         );
 
         getServer().getPluginManager().registerEvents(CustomListen.CREEPER_LISTEN, this);
